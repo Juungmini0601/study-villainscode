f66eb07966f14a1689ddb82cf7a28579e2374b9f
김정민 <raon37910@gmail.com>
2025-02-10 16:31:59 +0900

    spring mvc, 타임리프 세팅

---
+++ spring-mvc/src/main/java/io/raon/web/SpringMVCExampleApplication.java
@@ -0,0 +1,11 @@
+package io.raon.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMVCExampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMVCExampleApplication.class, args);
+	}
+}
