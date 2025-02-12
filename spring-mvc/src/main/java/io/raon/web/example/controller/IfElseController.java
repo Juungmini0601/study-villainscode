60617f2f2d6e46d5610641d83ffe1c0f4a34c6b5
김정민 <raon37910@gmail.com>
2025-02-12 16:23:46 +0900

    2일차 정리 - 반복문 조건문 템플릿 추가

--- spring-mvc/src/main/java/io/raon/web/example/controller/IfElseController.java
+++ spring-mvc/src/main/java/io/raon/web/example/controller/IfElseController.java
@@ -9,16 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import io.raon.web.example.model.User;
 
 @Controller
-public class ForController {
+public class IfElseController {
 
-	@GetMapping("/for/example1")
-	public String forExample1(Model model) {
+	@GetMapping("/ifelse/example1")
+	public String ifElse(Model model) {
 		User userA = new User("userA", 10);
 		User userB = new User("userB", 20);
 		User userC = new User("userC", 30);
 
 		List<User> userList = List.of(userA, userB, userC);
 		model.addAttribute("userList", userList);
-		return "for/example1";
+		return "ifelse/example1";
 	}
 }
