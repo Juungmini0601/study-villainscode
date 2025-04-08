a77e52f9317056f8749df16065adb145776e7894
김정민 <raon37910@gmail.com>
2025-04-08 13:28:57 +0900

    TCP/HTTP 서버 예제

--- tcp-http-example/src/main/java/io/jungmini/tcp/v2/TCPEchoServerV2.java
+++ tcp-http-example/src/main/java/io/jungmini/tcp/v2/TCPEchoServerV2.java
@@ -1,4 +1,4 @@
-package io.jungmini.tcp.v1;
+package io.jungmini.tcp.v2;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -13,29 +13,33 @@ import lombok.extern.slf4j.Slf4j;
  * @author    : kimjungmin
  * Created on : 2025. 4. 7.
  *
- * v1: 소켓을 생성하여 데이터를 한 번만 주고 받는다.
+ * v2: 하나의 소켓과 연결되어 데이터를 계속 주고 받는다.
  */
 @Slf4j
-public class TCPEchoServerV1 {
+public class TCPEchoServerV2 {
 	public static final int PORT = 9999;
 
 	// nc localhost 9999
 	public static void main(String[] args) throws Exception {
 		try (
 			ServerSocket serverSocket = new ServerSocket(PORT);
-
 		) {
 			log.info("server started on port: {}", PORT);
 			try (
+				// Clinet 연결 대기
 				Socket socket = serverSocket.accept();
 				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			) {
-				String message = br.readLine();
-				System.out.println("message from client: " + message);
-				bw.write(message);
-				bw.newLine();
-				bw.flush();
+				log.info("client connected");
+				// 클라이언트 연결 완료 응답을 계속 주고 받음
+				while (true) {
+					String message = br.readLine();
+					System.out.println("message from client: " + message);
+					bw.write(message);
+					bw.newLine();
+					bw.flush();
+				}
 			}
 		}
 	}
